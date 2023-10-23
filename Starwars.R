9414a8476cf5a61a2d8f58df648865a53bfc3351
KoustavBarik <K.Barik2@ncl.ac.uk>
2023-10-23 14:21:42 +0100

    Add scatter plot of height vs mass

--- Starwars.R
+++ Starwars.R
@@ -6,3 +6,8 @@ starwars %>%
     count(species) %>%
    ggplot(aes(y = species, x = n)) +
    geom_col()
+
+starwars %>% ggplot(aes(x = height, y = mass, col = species)) + geom_point()
+
+starwars %>% slice_max(mass)
+
